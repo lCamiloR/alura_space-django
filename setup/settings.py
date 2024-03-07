d7a9865a78dd6506dbaca9625c7dde0b4ebf7293
Luccas <61637025+lCamiloR@users.noreply.github.com>
2024-03-07 19:15:51 -0300

    update

--- setup/settings.py
+++ setup/settings.py
@@ -81,23 +81,23 @@ WSGI_APPLICATION = "setup.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'alura_space',
-        'USER': str(os.getenv("MYSQL_USER")),
-        'PASSWORD': str(os.getenv("MYSQL_PASSWORD")),
-        'HOST': 'localhost',
-        'PORT': '3306'
+        "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.mysql',
+#        'NAME': 'alura_space',
+#        'USER': str(os.getenv("MYSQL_USER")),
+#        'PASSWORD': str(os.getenv("MYSQL_PASSWORD")),
+#        'HOST': 'localhost',
+#        'PORT': '3306'
+#    }
+#}
 
 
 # Password validation
